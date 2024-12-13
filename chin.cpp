9c2a9502b186c624c0e81aba23c63dbed0150359
kazu-321 <kzs321kzs@gmail.com>
2024-12-14 01:16:45 +0900

    [update] set_position関数を追加

--- chin.cpp
+++ chin.cpp
@@ -1,30 +1,46 @@
 #include "chin.hpp"
 
-chin::chin(double _long, double direction, double hardness, double thickness)
-        : long_(_long), direction_(direction), hardness_(hardness), thickness_(thickness) {
-    is_boron = false;
+chin::chin(double _long, double vertical_direction, double horizontal_direction, double hardness, double thickness)
+        : long_(_long), vertical_direction_(vertical_direction), horizontal_direction_(horizontal_direction), hardness_(hardness), thickness_(thickness) {
+    is_boron_ = false;
 }
 
 
 void chin::boron(){
-    if(is_boron){
-        std::cout << "You already boroned." << std::endl;
+    if(is_boron_){
+        std::cout << "<<<You already boroned.>>>" << std::endl;
         return;
     }
-    is_boron = true;
-    std::cout << "You boroned." << std::endl;
-    std::cout << "Your Chin info:" << std::endl;
-    std::cout << "long: " << long_ << std::endl;
-    std::cout << "direction: " << direction_ << std::endl;
-    std::cout << "hardness: " << hardness_ << std::endl;
-    std::cout << "thickness: " << thickness_ << std::endl;
+    is_boron_ = true;
+    std::cout << "<<<You boroned.>>>" << std::endl;
+    std::cout << "  Your Chin info:" << std::endl;
+    std::cout << "long: " << long_ <<  "cm" << std::endl;
+    std::cout << "direction:" << std::endl;
+    std::cout << "\tvertical: " << vertical_direction_ << "°" << std::endl;
+    std::cout << "\thorizontal: " << horizontal_direction_ << "°" << std::endl;
+    std::cout << "hardness: " << hardness_ << "%" << std::endl;
+    std::cout << "thickness: " << thickness_ << "%" << std::endl;
 }
 
 void chin::unboron(){
-    if(!is_boron){
-        std::cout << "You already unboroned." << std::endl;
+    if(!is_boron_){
+        std::cout << "<<<You already unboroned.>>>" << std::endl;
         return;
     }
-    is_boron = false;
-    std::cout << "You unboroned." << std::endl;
+    is_boron_ = false;
+    std::cout << "<<<You unboroned.>>>" << std::endl;
+}
+
+bool chin::is_boron(){
+    return is_boron_;
+}
+
+void chin::set_position(double vertical_direction, double horizontal_direction){
+    vertical_direction_ = vertical_direction;
+    horizontal_direction_ = horizontal_direction;
+    std::cout << "<<<You set position.>>>" << std::endl;
+    std::cout << "  Your Chin info:" << std::endl;
+    std::cout << "direction:" << std::endl;
+    std::cout << "\tvertical: " << vertical_direction_ << "°" << std::endl;
+    std::cout << "\thorizontal: " << horizontal_direction_ << "°" << std::endl;
 }
--- chin.hpp
+++ chin.hpp
@@ -6,14 +6,17 @@
 class chin {
 private:
     double long_;       // chinの長さ(cm)
-    double direction_;  // chinの向き(°、-180~180、左を正)
+    double vertical_direction_;  // chinの向き(°、縦、-180~180、上を正)
+    double horizontal_direction_;// chinの向き(°、横、-180~180、左を正)
     double hardness_;   // chinの硬さ(%、0~100)
     double thickness_;  // chinの太さ(%、0~100)
+    bool is_boron_;     // chinがboronされているか
 public:
-    chin(double _long, double direction, double hardness, double thickness);
-    bool is_boron;
+    chin(double _long, double vertical_direction, double horizontal_direction, double hardness, double thickness);
     void boron();
     void unboron();
+    bool is_boron();
+    void set_position(double vertical_direction, double horizontal_direction);
 };
 
 
